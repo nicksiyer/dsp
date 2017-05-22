ffaa46914e3594698ff3e5dca2bf311827b7345f
niyer170 <iyer.nikhil.s@gmail.com>
2017-05-21 21:10:18 -0400

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("AGAIN YOU WANT MORE LINES!?")
+print("WHAT DO YOU WANT FROM ME")
+print("actually the formatting in emacs is pretty cool, i think i prefer this one")
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print("a couple of lines of code")
+print("is this what you wanted me to write?")
+print("a couple is defined as two or more. so whats this for?")
